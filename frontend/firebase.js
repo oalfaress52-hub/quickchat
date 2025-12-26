608fd63ce14b021aec129f7839b911cf10c86e1f
oalfaress52-hub <oalfaress52@gmail.com>
2025-12-26 12:51:12 -0500

    Update Firebase SDK imports and configuration

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -1,17 +1,17 @@
-// Firebase SDKs
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// TODO: paste firebaseConfig here
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDynYqIvEb-lWjsWwV6x6gkXkQk_sR8dmo",
+  authDomain: "quickchat-ed02b.firebaseapp.com",
+  projectId: "quickchat-ed02b",
+  storageBucket: "quickchat-ed02b.firebasestorage.app",
+  messagingSenderId: "664727825006",
+  appId: "1:664727825006:web:3db33664d3525a2f3a147f"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const app = initializeApp(firebaseConfig);
