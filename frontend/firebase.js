7346e26ebf6b39fc288de5de32c176b7967d6718
oalfaress52-hub <oalfaress52@gmail.com>
2025-12-27 12:21:26 -0500

    Refactor Firebase initialization and add services
    Updated Firebase initialization to use compatibility mode and added Firebase services.

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -1,9 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// Firebase configuration (public, safe to expose)
 const firebaseConfig = {
   apiKey: "AIzaSyDynYqIvEb-lWjsWwV6x6gkXkQk_sR8dmo",
   authDomain: "quickchat-ed02b.firebaseapp.com",
@@ -13,5 +8,9 @@ const firebaseConfig = {
   appId: "1:664727825006:web:3db33664d3525a2f3a147f"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase (compat)
+firebase.initializeApp(firebaseConfig);
+
+// Firebase services
+const auth = firebase.auth();
+const db = firebase.firestore();
