ee5f2a76996745b490305da6c610d0200bf54b58
oalfaress52-hub <oalfaress52@gmail.com>
2025-12-27 22:04:15 -0500

    Update firebase.js

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -1,6 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDU3BOPdu427etC9mACyPIMqYXMUQo9w1E",
@@ -11,6 +9,4 @@ const firebaseConfig = {
   appId: "1:418934265102:web:38340c750b6db60d76335f"
 };
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+export const app = initializeApp(firebaseConfig);
