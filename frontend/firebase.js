6f6f89283c60850e5a1fd18f63f7e3eac5deb825
oalfaress52-hub <oalfaress52@gmail.com>
2025-12-28 11:20:12 -0500

    Update firebase.js

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -1,6 +1,7 @@
-import { initializeApp } from "firebase/app";
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.1/firebase-app.js";
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyDU3BOPdu427etC9mACyPIMqYXMUQo9w1E",
   authDomain: "quickchatii.firebaseapp.com",
   projectId: "quickchatii",
@@ -9,4 +10,5 @@ const firebaseConfig = {
   appId: "1:418934265102:web:38340c750b6db60d76335f"
 };
 
+// ✅ THIS IS THE IMPORTANT PART
 export const app = initializeApp(firebaseConfig);
