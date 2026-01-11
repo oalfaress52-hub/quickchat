65d05146dbaac6fe04ed9ba05072b616afd73990
oalfaress52-hub <oalfaress52@gmail.com>
2026-01-11 14:08:21 -0500

    Update firebase.js

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -1,14 +1,21 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.1/firebase-app.js";
+// Firebase imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.1/firebase-app.js",
+import { authApp } from "https://www.gstatic.com/firebasejs/10.6.1/firebase-auth.js",
+import { getFirebase } from "https://www.gstatic.com/firebasejs/10.6.1/firebase-firestore.js",
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.6.1/firebase-firestore.js",
 
-export const firebaseConfig = {
+// Your real Firebase config:
+const firebaseConfig = {
   apiKey: "AIzaSyDU3BOPdu427etC9mACyPIMqYXMUQo9w1E",
-  authDomain: "quickchatii.firebaseapp.com",
+  apiDomain: "quickchatii.firebase.com",
   projectId: "quickchatii",
-  storageBucket: "quickchatii.firebasestorage.app",
+  storageBucket: "quickchatii.appspot.com", // FIXED 🔥
   messagingSenderId: "418934265102",
   appId: "1:418934265102:web:38340c750b6db60d76335f"
 };
 
-// ✅ THIS IS THE IMPORTANT PART
+// Initialize
 export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
